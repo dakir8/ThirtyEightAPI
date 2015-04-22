25114b504fa5f203ff3c1da1500d99c4f4837f86
dakir8 <thioyq@gmail.com>
2015-04-23 00:00:23 +0800

    image size

--- application/controllers/Video.php
+++ application/controllers/Video.php
@@ -3,32 +3,22 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Video extends CI_Controller {
 
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see http://codeigniter.com/user_guide/general/urls.html
-	 */
+	public function __construct()
+	{
+		parent::__construct();
+		
+		$this->load->model('video_model');
+	}
+        
 	public function index()
 	{
-// 		$this->load->view('video');
-		$this->load->model('video_model');
 		$data['query'] = $this->video_model->get_entries(10);
 		echo json_encode($data);
 	}
 	
 	public function getVideos() {
-		$this->load->model('video_model');
-		$data['query'] = $this->video_model->get_entries(10);
+		
+		$data['videos'] = $this->video_model->get_entries(10);
 		echo json_encode($data);
 	}
 }
